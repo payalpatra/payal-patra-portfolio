a328411d57e4866ee747664a7ac476dffbfd22e5
Payal <payalpatra105@gmail.com>
2025-10-18 22:54:59 +0530

    Updated Resume Link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/drive/folders/18sXHPk1_DxG7mm77305eQ0-VpMp0JglJ"
+            href="https://drive.google.com/file/d/1PDXayKxC34EjI_ZF_SUG9OxPbIVP7UB8/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
